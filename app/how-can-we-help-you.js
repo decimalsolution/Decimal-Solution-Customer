e7a4c39fcadd90be06b821c794c6319872f6d48b
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2023-12-14 13:07:33 +0500

    Refactor contact information layout and styling

--- app/how-can-we-help-you.js
+++ app/how-can-we-help-you.js
@@ -28,60 +28,62 @@ const HowCanWeHelpYou = ({ contactInfo }) => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 w-[300px] flex flex-col">
-      <div
-        className={cn(
-          show ? "block" : "hidden",
-          "relative -top-2 bg-white border-2 border-primary rounded-md shadow-lg"
-        )}
-      >
-        <p className="bg-primary text-white font-medium text-sm py-2 px-4  shadow-lg z-[1000]">
-          Hello!
-          <br /> Feel Free To Contact Us or email us at{" "}
-          <a
-            href="mailto:info@decimalsolution.com"
-            className="text-white font-bold text-sm hover:underline"
-          >
-            info@decimalsolution.com
-          </a>
-        </p>
-        <div className="flex flex-col gap-4 items-center p-4">
-          <div className="flex gap-2 hover:underline">
-            <Image
-              src="/Social Icons/messenger.png"
-              alt="messenger"
-              width={20}
-              height={20}
-            />
-            <button
-              className="text-primary font-medium text-sm"
-              onClick={() => handleNavigate("messenger")}
+    <div className="relative z-[1000]">
+      <div className="fixed bottom-6 right-6 w-[300px] flex flex-col">
+        <div
+          className={cn(
+            show ? "block" : "hidden",
+            "relative -top-2 bg-white border-2 border-primary rounded-md shadow-lg"
+          )}
+        >
+          <p className="bg-primary text-white font-medium text-sm py-2 px-4 shadow-lg">
+            Hello!
+            <br /> Feel Free To Contact Us or email us at{" "}
+            <a
+              href="mailto:info@decimalsolution.com"
+              className="text-white font-bold text-sm hover:underline"
             >
-              Messenger
-            </button>
-          </div>
-          <div className="flex gap-2 hover:underline">
-            <Image
-              src="/Social Icons/whatsapp.png"
-              alt="WhatsApp"
-              width={20}
-              height={20}
-            />
-            <button
-              className="text-primary font-medium text-sm"
-              onClick={() => handleNavigate("whatsapp")}
-            >
-              WhatsApp
-            </button>
+              info@decimalsolution.com
+            </a>
+          </p>
+          <div className="flex flex-col gap-4 items-center p-4">
+            <div className="flex gap-2 hover:underline">
+              <Image
+                src="/Social Icons/messenger.png"
+                alt="messenger"
+                width={20}
+                height={20}
+              />
+              <button
+                className="text-primary font-medium text-sm"
+                onClick={() => handleNavigate("messenger")}
+              >
+                Messenger
+              </button>
+            </div>
+            <div className="flex gap-2 hover:underline">
+              <Image
+                src="/Social Icons/whatsapp.png"
+                alt="WhatsApp"
+                width={20}
+                height={20}
+              />
+              <button
+                className="text-primary font-medium text-sm"
+                onClick={() => handleNavigate("whatsapp")}
+              >
+                WhatsApp
+              </button>
+            </div>
           </div>
         </div>
+        <button
+          className="ml-auto border-2 border-primary text-primary bg-white font-medium text-sm py-2 px-4 rounded-md shadow-lg"
+          onClick={() => setShow(!show)}
+        >
+          How Can We Help You?
+        </button>
       </div>
-      <button
-        className="ml-auto border-2 border-primary text-primary bg-white font-medium text-sm py-2 px-4 rounded-md shadow-lg z-[1000]"
-        onClick={() => setShow(!show)}
-      >
-        How Can We Help You?
-      </button>
     </div>
   );
 };
