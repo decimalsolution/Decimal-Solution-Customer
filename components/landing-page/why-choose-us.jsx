170a4d582ed90d7d7536debd3ccf6db7bffb993a
Syed Abdullah Saad <syedabdullahsaad1@gmail.com>
2023-12-15 14:54:47 +0500

    Update styles and content in landing page components

--- components/landing-page/why-choose-us.jsx
+++ components/landing-page/why-choose-us.jsx
@@ -35,29 +35,30 @@ export default function WhyChooseUs() {
           </div>
           <div className="card card-2">
             <h4 className="text-md  md:text-lg lg:text-xl 2xl:text-2xl font-semibold">
-              Product Analysis
+              Wireframe or UI/UX Design
             </h4>
             <p className="text-sm md:text-base lg:text-md 2xl:text-lg">
-              We analyze your product on market trends and price value to
-              observe its life expectancy in the market.
+              We suggest the solution or modifications to your product by
+              employing UI/UX design to make it interactive and creative.
             </p>
           </div>
           <div className="card card-3">
             <h4 className="text-md  md:text-lg lg:text-xl 2xl:text-2xl font-semibold">
-              Product Analysis
+              Product Development
             </h4>
             <p className="text-sm md:text-base lg:text-md 2xl:text-lg">
-              We analyze your product on market trends and price value to
-              observe its life expectancy in the market.
+              We develop a high-quality software product according to your
+              desire which compliments your business well.
             </p>
           </div>
           <div className="card card-4">
             <h4 className="text-md  md:text-lg lg:text-xl 2xl:text-2xl font-semibold">
-              Product Analysis
+              Product Testing
             </h4>
             <p className="text-sm md:text-base lg:text-md 2xl:text-lg">
-              We analyze your product on market trends and price value to
-              observe its life expectancy in the market.
+              Our team performs strict product testing to ensure reliability,
+              quality, cost-effectiveness, and product life expectancy while
+              reducing customers' aftercare needs.
             </p>
           </div>
         </div>
