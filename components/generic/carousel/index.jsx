360a676ae97d1455808668e4e0d2b917a04df645
MuhammadAlyan1 <alyan0332@gmail.com>
2024-04-25 12:40:37 +0500

    give accessable names to prev and next carousel buttons

--- components/generic/carousel/index.jsx
+++ components/generic/carousel/index.jsx
@@ -21,12 +21,13 @@ export default function Carousel({ children, ...props }) {
   }, [swiperRef]);
 
   return (
-    <div className="h-[200px] sm:h-[240px] md:h-[260px] lg:h-[300px] xl:h-[340px] 2xl:h-[380px] w-full max-w-[1750px] relative px-8">
+    <div className="relative h-[200px] w-full max-w-[1750px] px-8 sm:h-[240px] md:h-[260px] lg:h-[300px] xl:h-[340px] 2xl:h-[380px]">
       <button
+        aria-label="Previous carousel slide"
         onClick={goPrev}
-        className="absolute top-1/2 left-4 lg:left-0 z-10 transform -translate-y-1/2 bg-primary text-white w-8 h-8  2xl:w-16 2xl:h-16 grid place-items-center rounded-full"
+        className="absolute left-4 top-1/2 z-10 grid h-8 w-8 -translate-y-1/2 transform place-items-center rounded-full  bg-primary text-white lg:left-0 2xl:h-16 2xl:w-16"
       >
-        <ArrowLeft strokeWidth={3} className="w-3/4 h-3/4" />
+        <ArrowLeft strokeWidth={3} className="h-3/4 w-3/4" />
       </button>
       <Swiper
         spaceBetween={30}
@@ -48,9 +49,10 @@ export default function Carousel({ children, ...props }) {
       </Swiper>
       <button
         onClick={goNext}
-        className="absolute top-1/2  right-4 lg:right-0 z-10 transform -translate-y-1/2 bg-primary text-white w-8 h-8 2xl:w-16 2xl:h-16 grid place-items-center rounded-full"
+        aria-label="Next carousel slide"
+        className="absolute right-4  top-1/2 z-10 grid h-8 w-8 -translate-y-1/2 transform place-items-center rounded-full bg-primary text-white lg:right-0 2xl:h-16 2xl:w-16"
       >
-        <ArrowRight strokeWidth={3} className="w-3/4 h-3/4" />
+        <ArrowRight strokeWidth={3} className="h-3/4 w-3/4" />
       </button>
     </div>
   );
