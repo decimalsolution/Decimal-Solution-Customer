cfbe620bce1efccc32e32a5a4369282e22bdd939
AbdullahSaad5 <syedabdullahsaad1@gmail.com>
2024-05-08 18:03:22 +0500

    chore: Update Google Analytics tracking code

--- app/google-tag.js
+++ app/google-tag.js
@@ -5,19 +5,14 @@ const GoogleTag = ({ ga_id }) => {
   return (
     <>
       <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?
-    id=${ga_id}`}
-      ></Script>
-      <Script
         id="google-analytics"
         dangerouslySetInnerHTML={{
           __html: `
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-
-        gtag('config', '${ga_id}');
+          function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${ga_id}');
       `,
         }}
       ></Script>
